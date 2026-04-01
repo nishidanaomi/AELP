b1fabe02e7831745ba47675f4f2b3b1a60c4d180
N. <dnaominishida@gmail.com>
2026-04-01 12:14:47 -0300

    exemplos concluídos

---
+++ Aula07/exemplo_b.js
@@ -0,0 +1,13 @@
+// referência ao form e ao elemento h3 (onde será exibida resposta)
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+// "ouvinte" do evento, acionado pelo botão submit
+frm.addEventListener("submit", (e) => {
+const quilo = Number(frm.inQuilo.value);   // obtém os dados dos campos e transforma em numero
+const consumo = Number(frm.inConsumo.value);
+const valor = (quilo/1000) * consumo; // calcula
+resp.innerText = `Valor a pagar R$ ${valor.toFixed(2)}` // exibe resposta
+
+e.preventDefault() //previne o reload da página (defaut)
+})
