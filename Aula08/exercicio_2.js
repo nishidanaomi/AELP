2852e466f7df375d88f163fc7898f5e399456ee0
N. <dnaominishida@gmail.com>
2026-04-07 00:10:12 -0300

    exercicio 2 concluído

--- Aula08/exercicio_2.js
+++ Aula08/exercicio_2.js
@@ -8,10 +8,13 @@ e o número de quadriênios a que o funcionário tem direito.
 const prompt = require('prompt-sync')()
 const salario = Number(prompt('Salário (R$): '))
 const tempo = Number(prompt('Tempo (anos): '))
+if (isNaN(salario) || salario <= 0 || isNaN(tempo) || tempo < 0) {
+    console.log("Erro: Por favor, digite um preço numérico válido.")
+} else {
 const quadrienio = Math.floor(tempo / 4)
-
-const salarioFinal = (quadrienio * 0.01) * Number(salario) + Number(salario)
-const resultado = salarioFinal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+const acrescimo = quadrienio * 0.01
+const salarioFinal = salario * (1 + acrescimo)
 
 console.log(`Quadriênios: ${quadrienio}`)
-console.log(`Salário Final: ${resultado}`)
+console.log(`Salário Final: ${salarioFinal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`)
+}
