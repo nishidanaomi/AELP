a33905d429d4dd85486ccb1b959abf349cabe2af
N. <dnaominishida@gmail.com>
2026-04-06 23:54:52 -0300

    exercicio 1 finalizado

--- Aula08/exercicio_1.js
+++ Aula08/exercicio_1.js
@@ -3,17 +3,21 @@ Um supermercado está com uma promoção. Para aumentar suas vendas no
 setor de higiene, cada etiqueta de produto deve exibir uma mensagem anunciando
 50% de desconto (para um item) na compra de três unidades do produto. Elaborar
 um programa que leia descrição e preço de um produto. Após, apresente as
-mensagens indicando a promoção – conforme exibido a seguir
+mensagens indicando a promoção.
 */
 
 const prompt = require("prompt-sync")()
 const produto = prompt(`Produto: `)
-const preco = prompt(`Preço: R$ `)
+const preco = Number(prompt(`Preço: R$ `))
 
-const promocao = Number(preco * 0.5).toFixed(2)
-const total = Number((2 * preco) + promocao).toFixed(2)
+if (isNaN(preco) || preco <= 0) {
+    console.log("Erro: Por favor, digite um preço numérico válido.")
+} else {
+const promocao = preco * 0.5
+const total = (2 * preco) + promocao
 
-console.log(`Produto: ${produto}
-Preço: R$ ${preco}
-${produto} - Promoção: Leve 3 por R$ ${total}
-O 3º produto custa apenas R$ ${promocao}`)
+console.log(`Produto: ${produto}`)
+console.log(`Preço: ${preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`)
+console.log(`${produto} - Promoção: Leve 3 por ${total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`)
+console.log(`O 3º produto custa apenas ${promocao.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`)
+}
