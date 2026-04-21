dce01afb7abd0db3ca784f1c5856bef3b77fe255
N. <dnaominishida@gmail.com>
2026-04-21 13:53:02 -0300

    tarefa 3 concluida

--- Aula09/tarefa_3.js
+++ Aula09/tarefa_3.js
@@ -2,4 +2,25 @@
 
 const prompt = require("prompt-sync")()
 
-const 
+const entrada = prompt("Valor de saque: R$ ")
+const saqueSolicitado = Number(entrada)
+
+if (isNaN(saqueSolicitado || saqueSolicitado !== 0 || saqueSolicitado % 10 <= 0 )) {
+    console.log("Valor para saque inválido para notas disponíveis (R$ 10, 50, 100)")
+} else {
+    const notas100 = Math.floor(saqueSolicitado / 100)
+    const resto100 = saqueSolicitado % 100
+    const notas50 = Math.floor(resto100 / 50)
+    const resto50 = resto100 % 50
+    const notas10 = Math.floor(resto50 / 10)
+
+    if (notas100 > 0) {
+        console.log(`Notas de R$100: ${notas100}`)
+    }
+    if (notas50 > 0) {
+        console.log(`Notas de R$50: ${notas50}`)
+    }
+    if (notas10 > 0) {
+        console.log(`Notas de R$10: ${notas10}`)
+    }
+}
