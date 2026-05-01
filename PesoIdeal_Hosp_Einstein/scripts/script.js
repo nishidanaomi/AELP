1b52e6b39492ee914fc846957d43a6d67f88ad95
N. <dnaominishida@gmail.com>
2026-05-01 20:41:35 -0300

    refatorando html e começando o css

--- PesoIdeal_Hosp_Einstein/scripts/script.js
+++ PesoIdeal_Hosp_Einstein/scripts/script.js
@@ -1,28 +1,34 @@
-// Cria referência ao form e elemento onde será exibida a resposta
+// cria referência ao form e elemento onde será exibida a resposta
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-// "Ouvinte" de evento (porteiro), aciionando quando o botão submit for clicado
+// "ouvinte" de evento (porteiro), aciionando quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
-    e.preventDefault()          // Cancela o comportamento padrão (o envio do formulário/recarregamento)
+    e.preventDefault()          // cancela o comportamento padrão (o envio do formulário/recarregamento)
 
+    // aqui estamos pegando os dados digitados no form do HTML (input)
     const nome = frm.inNome.value
     const Masculino = frm.inMasculino.checked
     const altura = Number(frm.inAltura.value)
+    const peso = Number(frm.inPeso.value)
 
-    let peso                                // declara a variável peso
+    let pesoIdeal                                // declara a variável pesoIdeal
     if (Masculino) {                        // se masculino (ou, if === true)
-        peso = 52 + (0.75 * (altura - 152.4))
+        pesoIdeal = 52 + (0.75 * (altura - 152.4))
     } else {
-        peso = 49 + (0.67 * (altura - 152.4))
+        pesoIdeal = 49 + (0.67 * (altura - 152.4))
     }
-    
 
-    // Apresenta a resposta (altera o conteúdo do elemento h3 da página)
-    resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(3)} kg.`
-    resp.innerText = `E seu peso ideal ajustado é ${peso.toFixed(3)} kg.`
+    let pesoAjustado =  ((peso - pesoIdeal) * 0.25) + pesoIdeal  // seguindo as instruções da tabela
+
+    // apresenta a resposta (altera o conteúdo do elemento h3 da página)
+    outNome.innerText = `${nome}: `
+    outIdeal.innerText = `Seu peso ideal é ${pesoIdeal.toFixed(2).replace(".",",")} kg.`  // troquei o ponto por vírgula na resposta do peso
+    outAjustado.innerText =`Seu peso ideal ajustado é ${pesoAjustado.toFixed(2).replace(".",",")} kg.`     
 })
    // limpa o conteúdo do elemento h3 que exibe a resposta
     frm.addEventListener("reset", () => {
-    resp.innerText = ""
+    outNome.innerText = ""
+    outIdeal.innerText = ""
+    outAjustado.innerText = ""
     })   
