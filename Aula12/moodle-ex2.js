22ba6ad519faff2e77393a0ced1fac5815e4df8d
N. <dnaominishida@gmail.com>
2026-05-16 21:28:21 -0300

    #L1 - ex 2

--- Aula12/moodle-ex2.js
+++ Aula12/moodle-ex2.js
@@ -6,3 +6,11 @@ const input = process.platform === "win32"
 
 const valores = input.trim().split(/\s+/);
 
+const diasDaSemana = ["domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado"];
+
+for (let i = 0; i < valores.length; i++) {
+    let numDia = Number(valores[i]);
+    let diaPorExtenso = diasDaSemana[numDia -1];
+
+    console.log(diaPorExtenso)
+}
