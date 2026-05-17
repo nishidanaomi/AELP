da0b07df5d9b1e866f13e940c1b2d41e67f7b7fd
N. <dnaominishida@gmail.com>
2026-05-16 21:09:41 -0300

    #L1 - ex 1

--- Aula12/moodle-ex1.js
+++ Aula12/moodle-ex1.js
@@ -6,6 +6,18 @@ const input = process.platform === "win32"
 
 const valores = input.trim().split(/\s+/);
 
-const a = Number(valores.shift())
-const b = Number(valores.shift())
-const c = Number(valores.shift())
+const a = Number(valores.shift());
+const b = Number(valores.shift());
+const c = Number(valores.shift());
+
+let media = (a+b+c)/3;
+let numeros = [a, b, c];
+let contador = 0;
+
+for (let i= 0; i <3; i++) {
+    if (numeros[i]> media) {
+        contador++;
+    }
+}
+
+console.log(contador)
---
+++ Aula12/moodle-ex2.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const input = process.platform === "win32"
+    ? fs.readFileSync("entradas.txt", "utf8")
+    : fs.readFileSync("/dev/stdin", "utf8");
+
+const valores = input.trim().split(/\s+/);
+
